64dd6f054dbc5484b216e249ca116399becd9fbf
Saeed Salam <SaeedSalam@users.noreply.github.com>
2022-01-25 00:45:11 +0530

    script changes

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,3 +7,11 @@ function userDropdownToggle(e) {
   var dropToggler = document.querySelectorAll(".dropdown-box")[0];
   dropToggler.classList.toggle("showDropdown");
 }
+document.addEventListener("click", function (event) {
+  var dropMenuItem = document.querySelectorAll(".user-dropdown")[0];
+  var dropToggler = document.querySelectorAll(".dropdown-box")[0];
+  var isClickInsideMenu = dropMenuItem.contains(event.target);
+  if (!isClickInsideMenu) {
+    dropToggler.classList.remove("showDropdown");
+  }
+});
---
+++ assets/js/share-app.js
@@ -0,0 +1,27 @@
+var rightButtons = document.querySelectorAll(".right-buttons")[0];
+var shareBox = document.querySelectorAll(".share-box")[0];
+function shareBoxToggle() {
+  shareBox.classList.toggle("show-share-box");
+}
+document.addEventListener("click", function (event) {
+  var isClickInsideRightButtons = rightButtons.contains(event.target);
+  if (!isClickInsideRightButtons) {
+    shareBox.classList.remove("show-share-box");
+  }
+});
+
+var copyText = document.querySelectorAll(".share-url")[0];
+var copiedDiv = document.querySelectorAll(".share-copied")[0];
+var copyTimer = setTimeout(function () {
+  copiedDiv.classList.remove("show-copied");
+}, 800);
+function copyLink() {
+  var copiedContent = copyText.textContent.replace(/\s+/g, " ").trim();
+
+  navigator.clipboard.writeText(copiedContent);
+  clearTimeout(copyTimer);
+  copiedDiv.classList.add("show-copied");
+  copyTimer = setTimeout(function () {
+    copiedDiv.classList.remove("show-copied");
+  }, 800);
+}
